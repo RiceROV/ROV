653e0b8665ae477d325cfca3620cd1592e315104
Spencer Darwall <spencerdarwall@gmail.com>
2023-09-25 22:24:40 -0500

    Fixed and improved send functionality

--- laptop/src/keyboard_input.py
+++ laptop/src/keyboard_input.py
@@ -4,7 +4,7 @@ import socket
 
 def main(stdscr):
     # Connect to the Raspberry Pi 
-    # client_socket = connect_to_pi()
+    client_socket = connect_to_pi()
 
     # Set up the screen
     curses.curs_set(0)
@@ -21,41 +21,46 @@ def main(stdscr):
     while True:
         # Get user input
         key = stdscr.getch()
-        
+        cmd = ""
         # Process arrow keys
         if key == curses.KEY_RIGHT:
             x = min(x + 1, max_x - 1)
+            cmd = "1"
         elif key == curses.KEY_LEFT:
             x = max(x - 1, 0)
+            cmd = "2"
         elif key == curses.KEY_DOWN:
             y = min(y + 1, max_y - 1)
+            cmd = "3"
         elif key == curses.KEY_UP:
             y = max(y - 1, 0)
+            cmd = "4"
+        elif key == ord('/'):
+            cmd = "5"
+        elif key == ord(' '):
+            cmd = "6"
         elif key == ord('q'):
             break  # Quit the program on 'q' key
-        
+        client_socket.sendall(cmd.encode('utf-8'))
         stdscr.clear()
         stdscr.addch(y, x, 'X')
         stdscr.refresh()
-        
-        time.sleep(0.001)  # Sleep for 0.1 seconds
+        x = max_x // 2
+        y = max_y // 2
+        time.sleep(0.01)  # Sleep for 0.1 seconds
 
 def connect_to_pi():
-    host = '0.0.0.0'  # Listen on all available network interfaces
-    port = 12345  # Choose a port number
+    host = '168.5.177.28'  # Listen on all available network interfaces
+    port = 20001  # Choose a port number
 
     # Create a socket object
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to the host and port
-    server_socket.bind((host, port))
+    client_socket.connect((host, port))
 
-    # Listen for incoming connections (up to 5 clients in the queue)
-    server_socket.listen(5)
+    print(f"Established connection at {host}")
 
-    print(f"Server listening on {host}:{port} for the pi to connect")
-    client_socket, addr = server_socket.accept()
-    print(f"Accepted connection from {addr}")
     return client_socket
 
 curses.wrapper(main)
